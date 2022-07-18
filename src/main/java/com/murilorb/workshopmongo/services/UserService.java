562559f0d2928ff1d4607ad81d0cbed958e7171b
mucarb <murilo.rb60@gmail.com>
2022-07-18 20:31:54 -0300

    User insertion with POST

--- src/main/java/com/murilorb/workshopmongo/services/UserService.java
+++ src/main/java/com/murilorb/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.murilorb.workshopmongo.domains.User;
+import com.murilorb.workshopmongo.dtos.UserDTO;
 import com.murilorb.workshopmongo.repositories.UserRepository;
 import com.murilorb.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
