3ca38a40d4640259f023cb4366889a447ce54aed
mucarb <murilo.rb60@gmail.com>
2022-07-18 19:30:38 -0300

    Database instantiation operation

---
+++ src/main/java/com/murilorb/workshopmongo/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.murilorb.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.murilorb.workshopmongo.domains.User;
+import com.murilorb.workshopmongo.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.deleteAll();
+
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com");
+		User u3 = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+	}
+
+}
