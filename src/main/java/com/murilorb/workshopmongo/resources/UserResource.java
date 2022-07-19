5dd28c290c0234ff797f3d57ecc7682430176ffd
mucarb <murilo.rb60@gmail.com>
2022-07-19 13:54:58 -0300

    Endpoint to return a user posts

--- src/main/java/com/murilorb/workshopmongo/resources/UserResource.java
+++ src/main/java/com/murilorb/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.murilorb.workshopmongo.domains.Post;
 import com.murilorb.workshopmongo.domains.User;
 import com.murilorb.workshopmongo.dtos.UserDTO;
 import com.murilorb.workshopmongo.services.UserService;
@@ -60,6 +61,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 
 }
