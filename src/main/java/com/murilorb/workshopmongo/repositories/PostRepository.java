e751f4528d2a6cb7b921987cce4116870009cf14
mucarb <murilo.rb60@gmail.com>
2022-07-19 15:03:47 -0300

    Simple query with @Query

--- src/main/java/com/murilorb/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/murilorb/workshopmongo/repositories/PostRepository.java
@@ -3,10 +3,14 @@ package com.murilorb.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.murilorb.workshopmongo.domains.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
